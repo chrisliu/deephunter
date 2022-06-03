8953b6c1a0956874fa89ed04c8a65a84219a0de2
Christopher Liu <chrisliu@cs.ucla.edu>
2022-06-03 10:22:08 -0700

    added BERT model

--- deephunter/test_example.sh
+++ deephunter/test_example.sh
@@ -6,21 +6,24 @@ id can be any number.
 
 Before using the new scripts, please install the xxhash by "pip install xxhash"
 '
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/random/kmnc/0 -model lenet5 -random 1 -max_iteration 200 -criteria kmnc &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/random/nbc/0 -model lenet5 -random 1 -max_iteration 200 -criteria nbc &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/uniform/nbc/0 -model lenet5 -criteria nbc -random 0 -select uniform -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/uniform/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select uniform -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/tensorfuzz/nbc/0 -model lenet5 -criteria nbc -random 0 -select tensorfuzz -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/tensorfuzz/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select tensorfuzz -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/nbc/0 -model lenet5 -criteria nbc -random 0 -select deeptest -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select deeptest -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select deeptest -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/nbc/0 -model lenet5 -criteria nbc -random 0 -select deeptest -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/prob/nbc/0 -model lenet5 -criteria nbc -random 0 -select prob -max_iteration 200 &
-python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/prob/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select prob -max_iteration 200 &
+
+FUZZ_ITERS=100000
+
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/random/kmnc/0 -model lenet5 -random 1 -max_iteration $FUZZ_ITERS -criteria kmnc &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/random/nbc/0 -model lenet5 -random 1 -max_iteration $FUZZ_ITERS -criteria nbc &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/uniform/nbc/0 -model lenet5 -criteria nbc -random 0 -select uniform -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/uniform/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select uniform -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/tensorfuzz/nbc/0 -model lenet5 -criteria nbc -random 0 -select tensorfuzz -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/tensorfuzz/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select tensorfuzz -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/nbc/0 -model lenet5 -criteria nbc -random 0 -select deeptest -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select deeptest -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select deeptest -max_iteration $FUZZ_ITERS &
+#python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/deeptest/nbc/0 -model lenet5 -criteria nbc -random 0 -select deeptest -max_iteration $FUZZ_ITERS &
+python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/prob/nbc/0 -model lenet5 -criteria nbc -random 0 -select prob -max_iteration $FUZZ_ITERS &
+python image_fuzzer.py  -i ../test_seeds/mnist_seeds  -o lenet5_out/prob/kmnc/0 -model lenet5 -criteria kmnc -random 0 -select prob -max_iteration $FUZZ_ITERS &
 wait
-python utils/CoveragePlot.py -i lenet5_out -type coverage -iterations 200 -o  results/coverage_plot.pdf
-python utils/CoveragePlot.py -i lenet5_out -type seedattack -iterations 200 -o  results/diverse_plot.pdf
-python utils/UniqCrashBar.py -i lenet5_out -iterations 200 -o  results/uniq_crash.pdf
+python utils/CoveragePlot.py -i lenet5_out -type coverage -iterations $FUZZ_ITERS -o  results/coverage_plot.pdf
+python utils/CoveragePlot.py -i lenet5_out -type seedattack -iterations $FUZZ_ITERS -o  results/diverse_plot.pdf
+python utils/UniqCrashBar.py -i lenet5_out -iterations $FUZZ_ITERS -o  results/uniq_crash.pdf
 echo 'Finish! Please find the results in the results directory.'
 
